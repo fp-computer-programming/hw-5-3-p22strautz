142211b03c57ca0db3ed9d0099e01a841bd5e041
p22strautz <p22strautz@fairfieldprep.org>
2021-11-02 10:20:27 -0400

    finish hw5-3

---
+++ hw5-3.py
@@ -0,0 +1,17 @@
+# Author: SCT (ADMG) 11/2/21
+
+import calendar
+
+# Question 1
+calendar.TextCalendar().pryear(2020)
+
+# Question 2
+calendar.TextCalendar(6).pryear(2020)
+
+# Question 3
+calendar.TextCalendar().prmonth(2021, 6)
+
+# Question 4
+# N/A doesnt work
+
+# Question 5
